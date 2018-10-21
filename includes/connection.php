947e23f901f37ff4efc541ec34d9f84cb49a30f2
MalathSam1994 <43896805+MalathSam1994@users.noreply.github.com>
2018-10-21 20:59:30 +0300

    Update includes/connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,6 @@
 <?php
 // open connection 
-    $link = mysqli_connect("sql2.freemysqlhosting.net","sql2262244","cL8!lG8","sql2262244",3306);
+    $link = mysqli_connect("sql2.freemysqlhosting.net","sql2262244","cL8!lG5!","sql2262244",3306);
     if(!$link){
         die("cannot connect to server");
     }
