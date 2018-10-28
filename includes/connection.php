a9b6110164123471d8e9387bb9da45523f58b8a7
Black-Kokoro <44317973+Black-Kokoro@users.noreply.github.com>
2018-10-28 20:49:11 +0200

    Update includes/connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,7 @@
 <?php
 // open connection 
-    $link = mysqli_connect("sql2.freemysqlhosting.net","sql2262244","cL8!lG5!","sql2262244",3306);
+//    $link = mysqli_connect("sql2.freemysqlhosting.net","sql2262244","cL8!lG5!","sql2262244",3306);
+    $link = mysqli_connect("db4free.net","school123456","school987654321","school3265",3306);
     if(!$link){
         die("cannot connect to server");
     }
